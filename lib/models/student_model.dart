6234988d952fb3c1e06cb5f0141da8b7ffd2b994
fayezcoder <fayez.coder@gmail.com>
2023-04-11 08:56:22 +0300

    create StudentModel

---
+++ lib/models/student_model.dart
@@ -0,0 +1,24 @@
+class StudentModel {
+  String name;
+  String gender;
+  String id;
+  String nationality;
+  String schoolName;
+  DateTime admissionDate;
+  DateTime exitDate;
+  bool isNotificationEnabled;
+  int daysBeforeExit;
+
+  StudentModel({
+    required this.name,
+    required this.gender,
+    required this.id,
+    required this.nationality,
+    required this.schoolName,
+    required this.admissionDate,
+    required this.exitDate,
+    this.isNotificationEnabled = false,
+    this.daysBeforeExit = 1,
+  });
+
+}
